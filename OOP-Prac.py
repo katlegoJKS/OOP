a123924e47f6c5a76c206ae054e911c553b967e4
katlegoJKS <katlego.malatjie@umuzi.org>
2020-02-10 09:52:22 +0200

    first commit

---
+++ OOP-Prac.py
@@ -0,0 +1,16 @@
+class Person:
+
+
+    def __init__(self,name,age,gender,interests):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.interests = interests
+
+    def hello(self):
+        print("Hello, my name is",self.name,"and I am",self.age,"years old. My interests are",self.interests)
+
+
+person = Person('Ryan',30,'male','being a hardarse,agile and ssd hard drives')
+greeting = person.hello()
+print(greeting)
