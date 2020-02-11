07bf25bf4e4c6f6589b553672046b5738348a97b
katlegoJKS <katlego.malatjie@umuzi.org>
2020-02-11 10:58:54 +0200

    final commit

--- Person.py
+++ Person.py
@@ -1,16 +1,16 @@
 class Person:
 
 
-    def __init__(self,name,age,gender,interests):
+    def __init__(self,name,age,gender,interests=[]):
         self.name = name
         self.age = age
         self.gender = gender
         self.interests = interests
 
     def hello(self):
-        print("Hello, my name is",self.name,"and I am",self.age,"years old. My interests are",self.interests)
+        interests = ",".join(self.interests)
+        return "Hello, my name is {} and I am {} years old. My interests are {} ".format(self.name,self.age,interests)
 
 
-person = Person('Ryan',30,'male','being a hardarse,agile and ssd hard drives')
-greeting = person.hello()
-print(greeting)
+person = Person("Ryan",30,"male",["being a hardarse,agile and ssd hard drives"])
+print(person.hello())
